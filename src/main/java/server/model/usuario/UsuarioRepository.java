ce074d65bf59a829dc8a6073c607b3d476656f61
melivasr <anavasquez@estudiantec.cr>
2023-11-12 19:24:09 -0600

    Manejo de errores

--- src/main/java/server/model/usuario/UsuarioRepository.java
+++ src/main/java/server/model/usuario/UsuarioRepository.java
@@ -1,11 +1,11 @@
 package server.model.usuario;
 
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import server.model.usuario.Usuario;
 
 @Repository
-public interface UsuarioRepository extends CrudRepository<Usuario, Integer> {
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
 
 }
